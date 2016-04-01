9741903a2e78fc81e768284fecdf2cb13ec66c62
jennyhappy722 <u10316052@go.utaipei.edu.tw>
2016-04-01 17:34:54 +0800

    add comment

--- U10316052Buble.java
+++ U10316052Buble.java
@@ -1,11 +1,13 @@
 //u10316052
 import java.util.*;
 public class U10316052Buble {
+	//input array
 	public static void bubble_sort(int[] arr) {
 		int i, j, temp, len = arr.length;
 		for (i = 0; i < len - 1; i++)
 			for (j = 0; j < len - 1 - i; j++)
 				if (arr[j] > arr[j + 1]) {
+					//swap
 					temp = arr[j];
 					arr[j] = arr[j + 1];
 					arr[j + 1] = temp;
